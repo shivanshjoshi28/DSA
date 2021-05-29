528c68e1480582d0ace969fd0879516d0ab52c3b
Shivansh <joshishivansh28012001@gmail.com>
2021-05-29 12:41:09 +0530

    Word Ladder question

---
+++ Codes/Graph/Questions Based On Graph/WordLadder.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#include <vector>
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define pb push_back
+#define MP make_pair
+#define endl "\n"
+#define boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define F first
+#define S second
+const int inf = (int)1e18;
+const int ninf = (int)-1e17;
+#define rep(a, b) for (int a = 0; a < b; a++)
+#define per(a, b) for (int a = b - 1; a >= 0; a--)
+#define FOR(it, start, end) for (auto it = start; it != end; it++)
+
+int WordLadder(string start, string end, set<string> &wordlist)
+{
+    queue<string> q;
+
+    q.push(start);
+    if (wordlist.find(end) == wordlist.end())
+        return 0;
+    else
+    {
+        int level = 0;
+        q.push(start);
+        while (!q.empty())
+        {
+            level++;
+            int no_of_node_at_sameLevel = q.size();
+            while (no_of_node_at_sameLevel != 0)
+            {
+                string topstring = q.front();
+                q.pop();
+                for (int i = 0; i < topstring.size(); i++)
+                {
+                    string temp = topstring;
+                    for (int j = 'a'; j <= 'z'; j++)
+                    {
+                        temp[i] = j;
+
+                        if (temp.compare(topstring) == 0)
+                            continue;
+                        else if (temp.compare(end) == 0)
+                            return level + 1;
+                        else if (wordlist.find(temp) != wordlist.end())
+                        {
+                            wordlist.erase(temp);
+                            q.push(temp);
+                        }
+                    }
+                }
+                no_of_node_at_sameLevel--;
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    boost;
+    string start, end;
+    set<string> wordlist;
+    cin >> start >> end;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string str;
+        cin >> str;
+        wordlist.insert(str);
+    }
+    cout << WordLadder(start, end, wordlist);
+    return 0;
+}
